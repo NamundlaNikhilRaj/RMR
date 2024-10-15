8d7ca7149369a9f8710a2dd3b2aaccdcbeed00e6
NamundlaNikhilRaj <namundla.nikhilraju@signpostindia.com>
2024-10-15 13:57:56 +0530

    zzz
    Co-Authored-By: Jagdishaxily <133153484+Jagdishaxily@users.noreply.github.com>

--- Assets/xxx.cs
+++ Assets/xxx.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class qwe : MonoBehaviour
+public class xxx : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
